a5316636b75d9f48c2a4d77538c62fdb9c14c578
Ajinkya <g24.ajinkya.wanarkar@gnkhalsa.edu.in>
2026-07-14 18:54:37 +0530

    Custom Iterators

---
+++ Day 6/p21.py
@@ -0,0 +1,24 @@
+# Creating Custom Iterators 
+
+class CountDown:
+    # Iterator that counts down from a number to 1
+    def __init__(self,start):
+        self.current = start
+        self.start = start
+
+    def __iter__(self):
+        """Return the Iterator Object itself ."""
+        return self
+
+    def __next__(self):
+        if self.current < 1:
+            raise StopIteration 
+
+        value = self.current
+        self.current -= 1
+        return value
+print("Countdown")
+countdown = CountDown(5) 
+print (next(countdown))
+print (next(countdown))
+print (next(countdown))
